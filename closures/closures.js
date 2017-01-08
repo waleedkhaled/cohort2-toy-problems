a1d9744435683c8cf7b9c9bae2a7e9e7c8ae2831
Fatima Hammami <en.f.h@live.com>
2017-01-08 09:00:05 +0300

    closures

---
+++ closures/closures.js
@@ -0,0 +1,15 @@
+/*
+In this problem set we will review closures. Before you freak out, remember that closure is
+just a function that returns another function.
+
+Problem 1:
+Write a function that accepts an exponent and returns back a function that accepts
+a number and takes it to the power of the exponent.
+
+Example:
+var square = pow(2);
+var cube = pow(3);
+
+square(3);//should return 9
+cube(3);//should return 27
+*/
